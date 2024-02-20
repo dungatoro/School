12350c56b453d6ed474d565f3ba3c32ad68cb58e
dungatoro <noahjbarker@icloud.com>
2024-02-20 11:28:40 +0000

    ruby notebook

--- data_structures/hashtable/notebook/ruby/notes.rb
+++ data_structures/hashtable/notebook/ruby/notes.rb
@@ -9,7 +9,22 @@ class Notebook
   end
 
   def find_tags(*tags)
-    matches = []
-    tags.each { |tag| matches << @tags[tag] }
+    tags.map { |tag| @tags[tag] }.flatten.uniq
+  end
+
+  def [](title)
+    @notes[title] || "No note call #{title}"
+  end
+
+  def insert(title, text, tags)
+    @notes[title] = text
+    tags.each { |t| @tags.key?(t) ? @tags[t] << title : @tags[t] = [title] }
   end
 end
+
+n = Notebook.new('')
+n.insert 'dogs', 'some dogs stuff', %w[animals pets]
+n.insert 'cats', 'some cats stuff', %w[animals pets devils]
+n.insert 'emus', 'some emus stuff', %w[creepy fake]
+puts n['dogs']
+puts n.find_tags('devils', 'creepy')
