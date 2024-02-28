55f2b795c4015267fcb3e5a0470f54b84b70c393
dungatoro <noahjbarker@icloud.com>
2024-02-28 21:35:38 +0000

    finalize python notey

--- data_structures/python/notey.py
+++ data_structures/python/notey.py
@@ -1,5 +1,6 @@
 import pickle
 import cmd
+import datetime as dt
 
 class Notebook(object):
     def __init__(self, file=""):
@@ -15,7 +16,7 @@ class Notebook(object):
             f.close()
 
     def add_note(self, title, text, *tags):
-        self.__notes[title] = text
+        self.__notes[title] = text, dt.datetime.now()
         for tag in tags:
             if self.__tags.get(tag):
                 self.__tags[tag].append(title)
@@ -23,7 +24,10 @@ class Notebook(object):
                 self.__tags[tag] = [title]
 
     def __getitem__(self, title):
-        return self.__notes.get(title) or f"No note called {title}"
+        if self.__notes.get(title):
+            return f"{self.__notes[title][0]}"
+        else:
+            return f"No note called {title}"
 
     def search_tags(self, *tags):
         matches = []
@@ -31,6 +35,11 @@ class Notebook(object):
             if titles := self.__tags.get(tag):
                 matches += titles
         return list(set(matches)) # remove duplicates
+
+    def titles(self):
+        items = [(k, v) for k, (_, v) in self.__notes.items()]
+        for title, date in sorted(items, key=lambda x: x[1], reverse=True):
+            print(f"* {date.strftime('%d/%m/%y')}\t{title}")
 
     def __pickle(self, file):
         with open(file, 'wb') as f:
@@ -49,6 +58,9 @@ class NotebookCli(cmd.Cmd):
     notebook = Notebook("notey.pickle")
     prompt = " >> "
 
+    def __parse(self, line):
+        return [s.strip() for s in line.split('"') if s.strip()]
+
     def postcmd(self, stop, line):
         self.notebook.save()
         return 
@@ -64,24 +76,51 @@ class NotebookCli(cmd.Cmd):
                 exit(0)
 
     def do_make_note(self, line):
-        """Create a new note"""
-        title, text, *tags = line.split(",")
-        tags = [s.strip() for s in tags]
-        self.notebook.add_note(title, text, *tags)
-        print(f"Note titled {title} added to notebook!")
+        """
+        Add or overwrite a note in the notebook.
+         ~> make_note "title" "text" "tag1" tag2" ...
+        """
+        try:
+            title, text, *tags = self.__parse(line)
+        except:
+            print("Invalid arguments")
+        else:
+            self.notebook.add_note(title, text, *tags)
+            print(f"Note titled {title} added to notebook!")
 
-    def do_search_title(self, title):
-        """Search by title"""
-        print(self.notebook[title])
+    def do_search_title(self, line):
+        """
+        Print the contents of a .
+         ~> search_title "title"
+        """
+        try: 
+            title, *_ = self.__parse(line)
+        except:
+            print("Invalid arguments")
+        else:
+            print(self.notebook[title])
 
     def do_search_tags(self, line):
-        """Search by tags"""
-        tags = [s.strip() for s in line.split(",")]
-        matches = self.notebook.search_tags(*tags)
-        if matches:
-            for match in matches: print(f"* {match}")
+        """
+        Search by tags.
+         ~> search_tags "tag1" "tag2" ...
+        """
+        try:
+            tags = self.__parse(line)
+        except:
+            print("Invalid arguments")
         else:
-            print("No matches found")
+            matches = self.notebook.search_tags(*tags)
+            if matches:
+                for match in matches: print(f"* {match}")
+            else:
+                print("No matches found")
+
+    def do_titles(self, _):
+        """
+        Print a list of all page titles.
+        """
+        self.notebook.titles()
 
 if __name__ == "__main__":
     NotebookCli().cmdloop("Welcome to your notebook! Type 'help' if needed.")
