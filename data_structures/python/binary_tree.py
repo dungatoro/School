fd5914a71c0f67266a18f659332c0bad762d053f
dungatoro <noahjbarker@icloud.com>
2024-03-03 17:31:45 +0000

    add order methods on tree

--- data_structures/python/binary_tree.py
+++ data_structures/python/binary_tree.py
@@ -19,11 +19,39 @@ class Node:
             else:
                 self.__r.append(n)
 
+    def pre_order(self):
+        order = [self.__n]
+        if self.__l != None:
+            order += self.__l.pre_order()
+        if self.__r != None:
+            order += self.__r.pre_order()
+        return order
+
+    def in_order(self):
+        order = []
+        if self.__l != None:
+            order += self.__l.in_order()
+        order.append(self.__n)
+        if self.__r != None:
+            order += self.__r.in_order()
+        return order
+
+    def post_order(self):
+        order = []
+        if self.__l != None:
+            order += self.__l.post_order()
+        if self.__r != None:
+            order += self.__r.post_order()
+        order.append(self.__n)
+        return order
+
     def __repr__(self):
         return f"{self.__n}({self.__l or '_'} {self.__r or '_'})"
 
-
 if __name__ == '__main__':
-    t = Node(12, 9, 7, 10, -4, 4, 62);
+    t = Node(40, 30, 50, 25, 35, 45, 60, 15, 28, 55, 70);
     print(t)
+    print(f"PRE:  {t.pre_order()}")
+    print(f"IN:   {t.in_order()}")
+    print(f"POST: {t.post_order()}")
 
